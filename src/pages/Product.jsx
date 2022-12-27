fc921272b44763c46979acc25e1b99e1bc7fb0cc
Maksim Dudarev <maxim.d132213@gmail.com>
2022-12-27 16:20:55 +0300

    detailed products till M specifications

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,8 +7,7 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 import axios from 'axios';
-import membrane from './../images/C80.jpg';
-import applicate from './../images/detailedApplicate/A1.png';
+
 
 const Product = () => {
 
@@ -19,7 +18,7 @@ const Product = () => {
   const [detailedProducts, setDetailedProducts] = useState();
 
   useEffect(() => {
-    axios.get('/mocks/DetailedProducts.json').then(response => {
+    axios.get('/mocks/detailedProductNew.json').then(response => {
       const data = response.data;
       setDetailedProducts(data);
     }).catch(err => console.log(err));
@@ -27,12 +26,22 @@ const Product = () => {
 
 
 
-  return (
+  // useEffect(() => {
+  //   if (detailedProducts) {
+  //     let currentProduct = detailedProducts[productID];
+  //     console.log(JSON.stringify(currentProduct.description[0].text));
+  //   }
+  // }, [detailedProducts]);
+
+
+
+
+  return detailedProducts ? (
     <div className={styles.product_main}>
       <div className={styles.product_wrapper}>
-        <h1 className={styles.product_title}>Powertex M</h1>
-        <p className={styles.product_description_short}>Гидро-ветрозащита</p>
-        <img src={membrane} alt="membrane" className={styles.product_img} />
+        <h1 className={styles.product_title}>{detailedProducts[productID].title}</h1>
+        <p className={styles.product_description_short}>{detailedProducts[productID].caption}</p>
+        <img src={detailedProducts[productID].img} alt="membrane" className={styles.product_img} />
         <div className={styles.product_tabs}>
           <Tabs disableUpDownKeys={true}>
             <TabList>
@@ -41,109 +50,53 @@ const Product = () => {
               <Tab><div className={styles.tabs_header}>Технические характеристики</div></Tab>
             </TabList>
             <TabPanel>
-              <div className={styles.product_description_wrapper}>
-                <div className={styles.product_description_title}>
-                  <h1>Супердиффузионная мембрана</h1>
-                  <h1>премиум класса POWERTEX M</h1>
-                </div>
-                <p className={styles.product_description}>
-                  <b>Супердиффузионная мембрана </b>— одна из современных и востребованных разновидностей гидро-ветрозащитных мембран. Основная функция — защита утеплителя в конструкции кровли и фасадов от
-                  разрушающего влияния внешних факторов (протечки кровли или фасада) и вывода излишков пара из
-                  конструкции здания. Мембрана выпускается поверхностной плотностью от 60 до 180г/м<sup>2</sup>. Стандартная
-                  ширина материала 1500 мм.Рабочие температуры эксплуатации от -40<sup>o</sup> до +80<sup>o</sup>, состав материала
-                  100% полипропилен.
-                </p>
-                <p className={styles.product_description}><b></b>
-                  <b>Супердиффузионная мембрана По структуре мембрана представляет собой</b> - внешние два слоя нетканого материала (с УФ-стабилизатором) защищают внутренний слой паропроницаемой пленки (функциональная полипропиленовая
-                  «дышащая» пленка, придающая мембране свойства высокой паропроницаемости и гидроизоляции)
-                  дублируются ультразвуковым способом скрепления — это и есть структура супердиффузионных
-                  мембран. Как показывает практика реально паропроницаемость мембран POWERTEX, составляет 2300-
-                  3500 г/м<sup>2</sup>/сутки, а в совокупности с высоким показателем гидроизоляции (более 2300 мм.вод.столба)
-                  обеспечивает 100% защиту утеплителя от промокания и тем самым исключает потерю теплоизоляци-
-                  онных свойств последнего, дополнительно защищая всю конструкцию здания от образования гнили и
-                  плесени!
-                </p>
-              </div>
+              {detailedProducts[productID].description.map(descr => {
+                return (
+                  <div className={styles.product_description_wrapper} key={descr.id}>
+                    <div className={styles.product_description_title}>
+                      <h1>{descr.title}</h1>
+                    </div>
+                    {/* DOMPURIFY */}
+                    <p className={styles.product_description} dangerouslySetInnerHTML={{ __html: descr.text }}></p>
+                  </div>
+                )
+              })}
             </TabPanel>
             <TabPanel>
               <div className={styles.applicate_wrapper}>
-                <div className={styles.applicate_description}>
-                  <h1>Применение материала <span>Powertex A</span> в конструкции укрепленной кровли</h1>
-                  <img src={applicate} alt="a1" />
-                  <p>Powertex применяется как подкровленная ветровлагозащитная мембрана
-                    вутеплённых кровлях суглом наклона свыше 35° с различными покрытиями:
-                    металлочерепица, натуральная черепица, мягкие битумные плитки, про-
-                    филированные листы и др. Устанавливается над утеплителем поверх стропил
-                    под обрешеткой. Служит для защиты утеплителя и несущих элементов от под
-                    кровельного конденсата и как дополнительная защита от ветра.
-                    <b style={{ color: "red" }}> Не может применяться в качестве временной кровли!</b>
-                  </p>
-                </div>
-                <div className={styles.applicate_description}>
-                  <h1>Применение материала <span>Powertex A</span> в конструкции укрепленной кровли</h1>
-                  <img src={applicate} alt="a1" />
-                  <p>Powertex применяется как подкровленная ветровлагозащитная мембрана
-                    вутеплённых кровлях суглом наклона свыше 35° с различными покрытиями:
-                    металлочерепица, натуральная черепица, мягкие битумные плитки, про-
-                    филированные листы и др. Устанавливается над утеплителем поверх стропил
-                    под обрешеткой. Служит для защиты утеплителя и несущих элементов от под
-                    кровельного конденсата и как дополнительная защита от ветра.
-                    <b style={{ color: "red" }}> Не может применяться в качестве временной кровли!</b>
-                  </p>
-                </div>
-                <div className={styles.applicate_description}>
-                  <h1>Применение материала <span>Powertex A</span> в конструкции укрепленной кровли</h1>
-                  <img src={applicate} alt="a1" />
-                  <p>Powertex применяется как подкровленная ветровлагозащитная мембрана
-                    вутеплённых кровлях суглом наклона свыше 35° с различными покрытиями:
-                    металлочерепица, натуральная черепица, мягкие битумные плитки, про-
-                    филированные листы и др. Устанавливается над утеплителем поверх стропил
-                    под обрешеткой. Служит для защиты утеплителя и несущих элементов от под
-                    кровельного конденсата и как дополнительная защита от ветра.
-                    <b style={{ color: "red" }}> Не может применяться в качестве временной кровли!</b>
-                  </p>
-                </div>
-
+                {detailedProducts[productID].applicate.map(item => {
+                  return (
+                    <div className={styles.applicate_description} key={item.id}>
+                      <h1>{item.title}</h1>
+                      <img src={item.img} alt="a1" />
+                      {/* DOMPURIFY */}
+                      <p dangerouslySetInnerHTML={{ __html: item.description }}></p>
+                    </div>
+                  )
+                })}
               </div>
             </TabPanel>
-
             <TabPanel>
               <div className={styles.specifications_wrapper}>
-                <h1>Технические характеристики</h1>
+                <h1>{detailedProducts[productID].specifications.title}</h1>
                 <div className={styles.specifications_table}>
-                  <div className={styles.specifications_table_row}>
-                    <span>Плотность</span>
-                    <span>80 г/м</span>
-                  </div>
-                  <div className={styles.specifications_table_row}>
-                    <span>Состав</span>
-                    <span>полипропелен</span>
-                  </div>
-                  <div className={styles.specifications_table_row}>
-                    <span>Разрывная нагрузка прод./попер., Н/5см </span>
-                    <span>177/29</span>
-                  </div>
-                  <div className={styles.specifications_table_row}>
-                    <span>Разрывная нагрузка прод./попер., Н/5см </span>
-                    <span>177/29</span>
-                  </div>
-                  <div className={styles.specifications_table_row}>
-                    <span>Разрывная нагрузка прод./попер., Н/5см </span>
-                    <span>177/29</span>
-                  </div>
-                  <div className={styles.specifications_table_row}>
-                    <span>Разрывная нагрузка прод./попер., Н/5см </span>
-                    <span>177/29</span>
-                  </div>
+                  {detailedProducts[productID].specifications.features.map(item => {
+                    return (
+                      <div className={styles.specifications_table_row} key={item.id}>
+                        {/* DOMPURIFY */}
+                        <span dangerouslySetInnerHTML={{ __html: item.feature }}></span>
+                        <span dangerouslySetInnerHTML={{ __html: item.value }}></span>
+                      </div>
+                    )
+                  })}
                 </div>
               </div>
             </TabPanel>
           </Tabs>
         </div>
-
       </div>
     </div>
-  )
+  ) : <div>loading</div>
 }
 
 export default Product;
