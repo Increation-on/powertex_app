0efa6717ec22ed81100699b7766e80ad9282406e
Maksim Dudarev <maxim.d132213@gmail.com>
2023-01-20 12:56:03 +0300

    add footer information

--- src/UI/Footer/Footer.jsx
+++ src/UI/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import styles from './../styles/footer.module.css';
+import { telephones } from '../../contstants';
 
 const Footer = () => {
 
@@ -7,10 +8,35 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.menu_container}>
-        <div className={styles.copyright}>
-          All rights reserved {year} ©
+
+        <div className={styles.footer_company_information}>
+          <div className={styles.footer_company_information_address}>
+            220075, Республика Беларусь, г. Минск ул. Инженерная д.4
+          </div>
+          <div className={styles.footer_company_information_email}>
+            E-mail: raidicsgroup@gmail.com
+          </div>
         </div>
+
+        <div className={styles.footer_worktime}>
+          <h1>Режим работы:</h1>
+          <div>ПН-ПТ: с 9:00 до 17:00</div>
+        </div>
+
+        <div className={styles.footer_phones}>
+          <span className={styles.footer_phones_icon}></span>
+          <div className={styles.footer_phones_numbers}>
+            <div>{telephones.tel1}</div>
+            <div>{telephones.tel2}</div>
+          </div>
+        </div>
+
       </div>
+
+      <div className={styles.copyright}>
+        All rights reserved 2022 - {year} ©
+      </div>
+
     </footer>
   )
 }
