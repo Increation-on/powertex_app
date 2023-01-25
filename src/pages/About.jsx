f77f0725148efefe7be81fefdb0bcf26e9d8ceca
Maksim Dudarev <maxim.d132213@gmail.com>
2023-01-25 18:32:53 +0300

    add media queries for about, fix queries for payment and delivery

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,31 +4,46 @@ const About = () => {
   return (
     <div className={styles.main_about}>
       <div className={styles.about_wrapper}>
-        <div className={styles.description}>
+
+        <div className={styles.about_description}>
           <h1>Raidics</h1>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          <br />Etiam luctus eleifend nibh ac posuere.
-          <br />Sed consequat dictum libero, in condimentum felis lobortis vitae. Vivamus commodo turpis suscipit, feugiat odio ac, ullamcorper diam. Sed quis quam in dui volutpat congue. Donec eleifend, tellus et pretium finibus, mauris lacus posuere lectus, nec venenatis erat erat vel mauris. In imperdiet, tellus sed porttitor dapibus, dolor mauris luctus lectus, hendrerit aliquet metus orci et libero. Phasellus quis felis sit amet lorem imperdiet lacinia vel ac enim. Nam varius augue vel tellus blandit, ut vehicula mauris fringilla.
-        </div>
-        <div className={styles.search}>
-          <div className={styles.search_description}>
-            КАК
-            <br />НАС
-            <br />НАЙТИ
-            <br />?
-            
+          <div className={styles.about_description_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <br />Etiam luctus eleifend nibh ac posuere.
+            <br />Sed consequat dictum libero, in condimentum felis lobortis vitae. Vivamus commodo turpis suscipit, feugiat odio ac, ullamcorper diam. Sed quis quam in dui volutpat congue. Donec eleifend, tellus et pretium finibus, mauris lacus posuere lectus, nec venenatis erat erat vel mauris. In imperdiet, tellus sed porttitor dapibus, dolor mauris luctus lectus, hendrerit aliquet metus orci et libero. Phasellus quis felis sit amet lorem imperdiet lacinia vel ac enim. Nam varius augue vel tellus blandit, ut vehicula mauris fringilla.
           </div>
         </div>
+
+        <div className={styles.about_address}>
+          <div className={styles.about_address_description}>
+            <div className={styles.about_address_description_row}>
+              НАШИ
+              <br />АДРЕСА:
+              <br />
+            </div>
+            <div className={styles.about_address_description_column}>НАШИ АДРЕСА:</div>
+          </div>
+
+          <div className={styles.about_address_item}>г. Минск, ул. Инженерная д.4 {`(офис)`}</div>
+          <div className={styles.about_address_item}>г. Минск, ул. Селицкого 113/2 {`(склад)`}</div>
+        </div>
+
         <div className={styles.yandex_map}>
-          <div className={styles.address}>
-            НАШ
-            <br />АДРЕС:
-            <br />➔
+          <div className={styles.about_search}>
+            <div className={styles.about_search_row}>
+              КАК
+              <br />НАС
+              <br />НАЙТИ
+              <br />?
+            </div>
+            <div className={styles.about_search_column}>КАК НАС НАЙТИ ?</div>
           </div>
           <iframe
             src="https://yandex.ru/map-widget/v1/?um=constructor%3Abafd9f5bde0de8846135c6d7f17ae55258cd75695b835d49e392abf286b826eb&amp;source=constructor"
-            width="500" height="600" frameborder="0"></iframe>
+            width="500" height="600" frameborder="0">
+          </iframe>
         </div>
+
       </div>
     </div>
   )
