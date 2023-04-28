e3537e414895a595210596a96c379fdb1330339d
Maksim Dudarev <maxim.d132213@gmail.com>
2023-04-28 14:26:55 +0300

    error page styling

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles/error.module.css';
+import { NavLink } from 'react-router-dom';
+import logo from './../images/logo3.png';
+
 
 const ErrorPage = () => {
   return (
-    <div>
-        ERROR!!!!!!!!! Страница не найдена!
-    </div>
+    <>
+      <div className={styles.error_wrapper}>
+        <div className={styles.error_logo}>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className={styles.error_404}>
+          404
+        </div>
+        <div className={styles.error_exist}>
+          К сожалению, такой страницы не существует!
+        </div>
+        <div className={styles.error_main_link}>
+          <button>
+            <NavLink to={'/main'}>Перейти на главную</NavLink>
+          </button>
+        </div>
+      </div>
+    </>
+
   )
 }
 
